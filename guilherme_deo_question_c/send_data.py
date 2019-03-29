2132c1c0c78a9cbdf53425af6914ff6666bab4d0
Guilherme Deo <guideo@gmail.com>
2019-03-28 21:42:44 -0300

    Changing send_data.py to receive something back from request

--- guilherme_deo_question_c/send_data.py
+++ guilherme_deo_question_c/send_data.py
@@ -3,8 +3,14 @@ import socket
 
 if __name__ == "__main__":
     while True:
-        data = input('Message to send: ')
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(('localhost', 8730))
-        print("Sent data: {}".format(sock.send(data.encode())))
-        sock.close()
+        try:
+            data = input('Message to send: ')
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect(('localhost', 8730))
+            sock.sendall(data.encode())
+            print("Sent data: {}".format(data))
+            data = sock.recv(1024)
+            print("Result: {}".format(data.decode()))
+            sock.close()
+        except ConnectionRefusedError as e:
+            print("CONNECTION REFUSED - Server is busy. \nERROR: {}".format(e))
